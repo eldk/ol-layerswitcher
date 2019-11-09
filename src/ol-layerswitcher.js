54563e18a81821a90c8762fa45795731e7836d10
eldk <webmaster@opalesurfcasting.net>
2019-11-09 22:10:20 +0100

    test

--- src/ol-layerswitcher.js
+++ src/ol-layerswitcher.js
@@ -43,7 +43,7 @@ export default class LayerSwitcher extends Control {
         button.setAttribute('class', 'layer-switcher-open-main')
         button.setAttribute('type', 'button');
         button.setAttribute('title', tipLabel);
-        buttonb.innerHTML = 'X';
+        button.innerHTML = 'X';
         element.appendChild(button);
 
         this.panel = document.createElement('div');
